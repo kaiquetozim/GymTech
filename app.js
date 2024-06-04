1d44d7ad8dd827b6c98a3d6b1621b8f8f277f4d1
kaiquetozim <kaique.tozim@sptech.school>
2024-06-04 02:33:23 -0300

    Versão final do projeto GymBody

--- app.js
+++ app.js
@@ -15,26 +15,14 @@ var HOST_APP = process.env.APP_HOST;
 
 var app = express();
 
-var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
-var avisosRouter = require("./src/routes/avisos");
-var medidasRouter = require("./src/routes/medidas");
-var aquariosRouter = require("./src/routes/aquarios");
-var empresasRouter = require("./src/routes/empresas");
 var dashboardRouter = require("./src/routes/dashboard");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
 app.use(cors());
-
-app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
-app.use("/avisos", avisosRouter);
-app.use("/medidas", medidasRouter);
-app.use("/aquarios", aquariosRouter);
-app.use("/empresas", empresasRouter);
 app.use("/dashboard", dashboardRouter);
 
 app.listen(PORTA_APP, function () {
--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -5,55 +5,90 @@
 /*
 comandos para mysql server
 */
+CREATE DATABASE gymBody;
 
-CREATE DATABASE aquatech;
+USE gymBody;
 
-USE aquatech;
-
-CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14)
+CREATE TABLE Cliente(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+email VARCHAR(256) UNIQUE,
+cpf CHAR(11),
+whatsapp CHAR(11)
 );
 
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE plano(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(10),
+preco DECIMAL(15.2)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+
+CREATE TABLE matricula(
+id INT PRIMARY KEY AUTO_INCREMENT,
+mes INT,
+ano INT,
+fk_cliente INT,
+fk_plano INT,
+FOREIGN KEY (fk_cliente) REFERENCES Cliente(id),
+FOREIGN KEY (fk_plano) REFERENCES plano(id)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE administrador(
+id INT PRIMARY KEY AUTO_INCREMENT,
+email VARCHAR(256) UNIQUE,
+senha VARCHAR(20)
 );
 
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
+INSERT INTO Cliente VALUES
+	(default, 'Jurandiro', 'jurandiro@gmail.com', '12345678910', '11982492957'),
+	(default, 'Marconildo', 'marconildo@gmail.com', '12345678911', '11982492951'),
+	(default, 'Aronilda', 'aronildo@gmail.com', '12345678912', '11982492952'),
+	(default, 'Closdovalda', 'closdovalda@gmail.com', '12345678913', '11982492953'),
+	(default, 'Jiane', 'jiane@gmail.com', '12345678914', '11982492954'),
+	(default, 'Jhon Lin', 'jhonlin@gmail.com', '12345678915', '11982492955'),
+	(default, 'Raniely', 'raniely@gmail.com', '12345678916', '11982492956'),
+    (default, 'Marcieli', 'marciele@gmail.com', '12345678917', '11982492958'),
+    (default, 'Ranivaldo', 'ranivaldo@gmail.com', '12345678918', '11982492959');
+    
+    
+INSERT INTO plano VALUES
+	(DEFAULT, 'Black', 150.00),
+	(DEFAULT, 'Gold', 110.00),
+	(DEFAULT, 'Silver', 85.00),
+	(DEFAULT, 'Bronze', 65.00);
+    
+INSERT INTO matricula VALUES	
+	(default, 1, 2024, 1, 1),
+	(default, 2, 2024, 2, 2),
+	(default, 3, 2024, 3, 3),
+	(default, 4, 2024, 4, 4),
+	(default, 5, 2024, 5, 1),
+	(default, 6, 2024, 6, 2),
+	(default, 4, 2024, 7, 2),
+	(default, 1, 2024, 8, 3),
+	(default, 2, 2024, 9, 3);
 
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
+    
+-- INSERINDO ADMINISTRADOR DA GYMBODY
+INSERT INTO administrador VALUES
+	(default, 'kaique@gmail.com', 'sotero0610');
+    
+-- TOTAL DE MATRICULAS NO MES DO ANO ATUAL
+SELECT count(id), mes FROM matricula WHERE ano = 2024 GROUP BY mes;
 
-insert into empresa (razao_social, cnpj) values ('Empresa 1', '00000000000000');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
+-- DASHBOARD MATRICULA POR PLANO
+SELECT count(matricula.id), nome FROM matricula AS matricula JOIN plano AS plano ON fk_plano = plano.id GROUP BY plano.id;
+
+-- ARRECADAÇÃO NO ANO ATUAL
+SELECT SUM(plano.preco) AS valor FROM matricula AS matricula JOIN plano AS plano ON fk_plano = plano.id
+	WHERE ano = 2024;
+    
+SELECT * FROM Cliente;
+SELECT * FROM matricula ORDER BY fk_plano;
+SELECT * FROM administrador;
+
+DELETE FROM plano WHERE id > 4;
+    
+ 
+   
